5135e59be18e8c6e9d421826c71132abeba5adc8
JonathanOikawa <joikawa@csumb.com>
2016-12-02 15:25:17 -0800

    Added all initial J2EE hangman files.

---
+++ cst6/src/main/java/jo/MyServlet.java
@@ -0,0 +1,92 @@
+package jo;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+public class MyServlet extends HttpServlet  {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		if (request.getParameter("image") != null) {
+			try{
+			     String fileName = request.getParameter("image");             
+			     FileInputStream fis = new FileInputStream(new File("C:/Users/oikaw/Desktop/Oikawacst6/cst6/src/main/resources/" + fileName));
+			     BufferedInputStream bis = new BufferedInputStream(fis);             
+			     response.setContentType("image/gif");
+			     BufferedOutputStream output = new BufferedOutputStream(response.getOutputStream());
+			     for (int data; (data = bis.read()) > -1;) {
+			    	 output.write(data);
+			     }             
+			  }
+			  catch(IOException e) {
+			
+			  } finally{
+			      // close the streams
+			  }
+		} else {
+			Hangman hangmanGame;
+			if (request.getSession().getAttribute("game") == null) {
+				hangmanGame = new Hangman();
+				request.getSession().setAttribute("game", hangmanGame);
+				request.getRequestDispatcher("hangman.jsp").forward(request, response);
+			} else {
+				hangmanGame = (Hangman)request.getSession().getAttribute("game");
+				String imagePath = Integer.toString(hangmanGame.getWrongGuesses() + 1);
+				request.setAttribute("image", imagePath);
+				if (hangmanGame.isGameWon() == true) {
+					request.getRequestDispatcher("hgwin.jsp").forward(request, response);
+				} else if (hangmanGame.getWrongGuesses() == 6) {
+					request.setAttribute("word", hangmanGame.getWord());
+					request.getRequestDispatcher("hglose.jsp").forward(request, response);
+				} else {
+					request.setAttribute("word", hangmanGame.getWord());
+					request.setAttribute("guess", hangmanGame.generateGuessString());
+					if (!hangmanGame.isNewGuess() || hangmanGame.isCorrectNewLetter()) {
+						request.getRequestDispatcher("hgcont.jsp").forward(request, response);
+					} else {
+						request.setAttribute("lastGuess", hangmanGame.getLastGuess());
+						request.getRequestDispatcher("hgcontbad.jsp").forward(request, response);
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Hangman hangmanGame = (Hangman)request.getSession().getAttribute("game");
+		hangmanGame.addGuess(request.getParameter("guess"));
+		HttpSession session = request.getSession();
+		doGet(request, response);
+	}
+
+}
---
+++ cst6/src/main/webapp/hgcontbad.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>HG ContBad</title>
+</head>
+<body>
+	<image src="MyServlet?image=h<%out.print(request.getAttribute("image"));%>.gif">
+	<h2 style=\"font-family:'Lucida Console', monospace\"> <% out.println(request.getAttribute("guess")); %></h2>
+	<form action="MyServlet" method="post">
+    <p>There are no <% out.println(request.getAttribute("lastGuess")); %>'s.
+    <input type="text" name="guess" /></p>
+
+    <p>Submit button.
+    <input type="submit" name="submit" value="submit" /></p>
+</form>
+</body>
+</html>
